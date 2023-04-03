7bae9fe4637d33e98ca80fe055c52c6e22b14b09
cdlesage <cdl23@live.unc.edu>
2023-04-03 03:33:30 -0400

    Question 1 table, selecting and sorting

--- hw4.R
+++ hw4.R
@@ -1,1 +1,15 @@
 library(tidyverse)
+
+# Reading in data
+data = read_csv("airport_pairs.csv")
+
+#Question 1
+# Filtering for data of flights in or out of RDU
+RDUdata = filter(data, origin == "RDU" | dest == "RDU")
+# Selecting the columns we need
+RDUdataselected = subset(RDUdata, select = c(origin, dest, passengers))
+# Sorting by passenger count
+q1dataarranged = arrange(RDUdataselected, passengers)
+# Filtering for more than 10000 passengers
+q1filtered = filter(q1dataarranged, passengers >= 10000)
+# ATL is most popular nonstop destination                 
