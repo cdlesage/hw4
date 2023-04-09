b23f788d66567dd68c10b7178a94d3b1e1e55b49
cdlesage <cdl23@live.unc.edu>
2023-04-09 17:36:20 -0400

    Rest of question 2: scatterplots

--- hw4.R
+++ hw4.R
@@ -10,34 +10,68 @@ RDUdata = filter(data, origin == "RDU" | dest == "RDU")
 # Selecting the columns we need
 RDUdataselected = subset(RDUdata, select = c(origin, dest, passengers))
 # Sorting by passenger count
-RDUdataarranged = arrange(RDUdataselected, passengers)
+RDUdataarranged = arrange(RDUdata, passengers)
 # Filtering for more than 10000 passengers
 filtered = filter(RDUdataarranged, passengers >= 10000)
 # ATL is most popular nonstop destination                 
 
 
 # Question 2
+# Loading ACS variables and saving as a CSV file to open in Excel
 acs_data = load_variables(2021, "acs5")
 write_csv(acs_data, "acsdata.csv")
 
-census = get_acs(
+# Making a table renamed for origin population
+census_origin = get_acs(
   geography="cbsa",  # could be tract, block group, etc.
   variables=c(
-    "total_mode"="B08301_001",
-    "drove_alone"="B08301_003",
-    "transit"="B08301_010",
-    "taxi"="B08301_016",
-    "motorcycle"="B08301_017",
-    "bicycle"="B08301_018",
-    "walk"="B08301_019",
-    "other"="B08301_020",
-    "wfh"="B08301_021",
-    "total_ttime"="B08303_001",
-    "ttime_60_89"="B08303_012",
-    "ttime_90plus"="B08303_013"
+    "household_income_origin"="B19013_001",
+    "food_stamps_origin"="B22003_001",
+    "atorabove_povertylevel_origin"="B17001_031",
+    "origin_pop"="B01003_001"
   ),
   year=2021,
-  state="NC",
   survey="acs5",
   output="wide"
 )
+census_origin = census_origin %>%
+  rename("origin_cbsa"="GEOID")
+
+# Making a table renamed for destination population
+census_dest = get_acs(
+  geography="cbsa",  
+  variables=c(
+    "household_income_dest"="B19013_001",
+    "food_stamps_dest"="B22003_001",
+    "atorabove_povertylevel_dest"="B17001_031",
+    "dest_pop"="B01003_001"
+  ),
+  year=2021,
+  survey="acs5",
+  output="wide"
+)
+census_dest = census_dest %>%
+  rename("dest_cbsa"="GEOID")
+
+# Merging all the data
+join1 = merge(filtered, census_origin, by="origin_cbsa")
+join2 = merge(join1, census_dest, by="dest_cbsa")
+
+# Scatterplot of origin population vs total passengers
+ggplot(join2, aes(x=origin_popE, y=passengers)) +
+  geom_point(size=1)
+
+# Scatterplot of destination population vs total passengers
+ggplot(join2, aes(x=dest_popE, y=passengers)) +
+  geom_point(size=1)
+
+# Scatterplot of flight distance vs total passengers
+ggplot(join2, aes(x=distancemiles, y=passengers)) +
+  geom_point(size=1)
+
+# Scatterplots of poverty status variable 
+ggplot(join2, aes(x=atorabove_povertylevel_originE, y=passengers)) +
+  geom_point(size=1)
+
+ggplot(join2, aes(x=atorabove_povertylevel_destE, y=passengers)) +
+  geom_point(size=1)
